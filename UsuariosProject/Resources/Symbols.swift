4f6fab7f394d6538d430c52bfb11fd3efc83d80a
Datafox-JCP <datafox@outlook.com>
2024-10-12 21:10:17 -0600

    Estructura general

---
+++ UsuariosProject/Resources/Symbols.swift
@@ -0,0 +1,14 @@
+//
+//  Symbols.swift
+//  UsuariosProject
+//
+//  Created by Juan Hernandez Pazos on 12/10/24.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
