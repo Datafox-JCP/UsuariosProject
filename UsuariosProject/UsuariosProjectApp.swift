2dafeb2c44f7179b8c58d0e258f874fcbb3a82a2
Datafox-JCP <datafox@outlook.com>
2024-10-12 20:38:28 -0600

    Initial Commit

---
+++ UsuariosProject/UsuariosProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsuariosProjectApp.swift
+//  UsuariosProject
+//
+//  Created by Juan Hernandez Pazos on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct UsuariosProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
